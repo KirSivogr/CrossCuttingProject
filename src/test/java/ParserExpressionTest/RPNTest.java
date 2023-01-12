4bf6444fa0562d1e5dcfe5f9a52bfdc3a6a10c67
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-12 17:19:42 +0300

    fixed name of tests and throw test

--- src/test/java/ParserExpressionTest/RPNTest.java
+++ src/test/java/ParserExpressionTest/RPNTest.java
@@ -9,7 +9,14 @@ import java.io.IOException;
 
 public class RPNTest {
     @Test
-    public void RPNTest1() throws Exception {
+    public void RPNTestShouldThrow() throws Exception {
+        String expression = "7/0";
+        Double expectedAnswer = 0.0;
+        RPN rpn = new RPN(expression);
+        Assertions.assertThrows(ArithmeticException.class, () -> rpn.RPNToAnswer());
+    }
+    @Test
+    public void RPNTestSimpleExpression() throws Exception {
         String expression = "3+21+3/3*0.5";
         Double expectedAnswer = 24.5;
         RPN rpn = new RPN(expression);
@@ -18,7 +25,7 @@ public class RPNTest {
     }
 
     @Test
-    public void RPNTest2() throws Exception {
+    public void RPNTestExpressionWithBrackets() throws Exception {
         String expression = "1+3+3/3*(0.5+0.5)";
         Double expectedAnswer = 5.0;
         RPN rpn = new RPN(expression);
@@ -27,7 +34,7 @@ public class RPNTest {
     }
 
     @Test
-    public void RPNTest3() throws Exception {
+    public void RPNTestExpressionWithZero() throws Exception {
         String expression = "0/1/1/1/1/1";
         Double expectedAnswer = 0.0;
         RPN rpn = new RPN(expression);
@@ -36,34 +43,11 @@ public class RPNTest {
     }
 
     @Test
-    public void RPNTest4() throws Exception {
-        String expression = "3*3*(3+3)";
-        Double expectedAnswer = 54.0;
-        RPN rpn = new RPN(expression);
-        Double RPNAnswer = rpn.RPNToAnswer();
-        Assertions.assertEquals(expectedAnswer, RPNAnswer);
-    }
-
-
-    @Test
-    public void RPNTest5() throws Exception {
+    public void RPNTestRationalExpression() throws Exception {
         String expression = "1+3*3+2+0/5+0.5";
         Double expectedAnswer = 12.5;
         RPN rpn = new RPN(expression);
         Double RPNAnswer = rpn.RPNToAnswer();
         Assertions.assertEquals(expectedAnswer, RPNAnswer);
-    }
-
-    @Test
-    public void RPNTest6() throws Exception {
-        String expression = "7/0";
-        Double expectedAnswer = 0.0;
-        RPN rpn = new RPN(expression);
-        try {
-            Double RPNAnswer = rpn.RPNToAnswer();
-            Assertions.assertEquals(expectedAnswer, RPNAnswer);
-        } catch (ArithmeticException thrown) {
-            Assertions.assertEquals("Division by zero!!!", thrown.getMessage());
-        }
     }
 }
