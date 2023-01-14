db5fccd6df2ebfb889c5465fc97e8982bb0fa78b
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-15 01:27:34 +0300

    refactor name of parametrs

--- src/main/java/archiving/ArchivingGen.java
+++ src/main/java/archiving/ArchivingGen.java
@@ -8,11 +8,11 @@ import java.util.zip.ZipOutputStream;
 
 public class ArchivingGen {
     public ArchivingGen() {}
-    public static void zipFile(String filename , String directoryname) throws java.io.IOException {
-        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(directoryname));
-            FileInputStream fis= new FileInputStream(filename);)
+    public static void zipFile(String fileName , String directoryName) throws java.io.IOException {
+        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(directoryName));
+            FileInputStream fis = new FileInputStream(fileName);)
         {
-            ZipEntry entry1=new ZipEntry(filename.substring(filename.lastIndexOf("\\") + 1));
+            ZipEntry entry1=new ZipEntry(fileName.substring(fileName.lastIndexOf("\\") + 1));
             zout.putNextEntry(entry1);
             byte[] buffer = new byte[fis.available()];
             fis.read(buffer);
@@ -23,11 +23,11 @@ public class ArchivingGen {
             System.out.println(ex.getMessage());
         }
     }
-    public static void unzipFile(String filename, String directoryname) throws java.io.IOException {
-        ZipInputStream zin = new ZipInputStream(new FileInputStream(filename));
+    public static void unzipFile(String fileName, String directoryName) throws java.io.IOException {
+        ZipInputStream zin = new ZipInputStream(new FileInputStream(fileName));
         ZipEntry entry;
         while((entry = zin.getNextEntry()) != null) {
-            FileOutputStream fout = new FileOutputStream(directoryname);
+            FileOutputStream fout = new FileOutputStream(directoryName);
             for (int c = zin.read(); c != -1; c = zin.read()) {
                 fout.write(c);
             }
