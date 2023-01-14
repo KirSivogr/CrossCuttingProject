5288acb51a8774b0a08cae6e36c1101f17764573
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-15 01:28:29 +0300

    rename method

--- src/main/java/processors/ProcessingFile.java
+++ src/main/java/processors/ProcessingFile.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface ProcessingFile {
     public List<String> readFromFile() throws Exception;
-    public void writeToFile(String inputFileName) throws Exception;
+    public void writeResultToFile(String inputFileName) throws Exception;
+
     public List<String> calculate(List<String> data) throws Exception;
+    public String getFileName();
 }
